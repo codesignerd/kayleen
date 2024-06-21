21c0b39b76d9186d6af1e43d6c332414c32d05fc
Blessing Abayomi <blessingabayomi0204@gmail.com>
2024-06-21 19:55:41 +0100

    implement Bootstrap in project

--- main.js
+++ main.js
@@ -2,27 +2,24 @@ const mobileMenu = document.querySelector(".hamburger-menu");
 const closeBtn = document.querySelector(".close-btn");
 const firtsNav = document.querySelector(".first-nav");
 const secondNav = document.querySelector(".second-nav");
-const minWidth = 780;
+const minWidth = 768;
+
+document.addEventListener("resize", () => {
+
 const screenWidth = window.innerWidth;
 
 if (minWidth >= screenWidth) {
-    firtsNav.style.marginBottom ="0";
-
-    if (closeBtn.style.display ="none") {
-        closeBtn.style.display ="block";
-    } else {
-        closeBtn.style.display ="none";
-    }
-
-    secondNav.style.display ="block";
-
-    mobileMenu.addEventListener("click", () => {
-        secondNav.style.transitionDuration = "600ms";
-        secondNav.style.left = 0;
-    });
     
-    closeBtn.addEventListener("click", () => {
-        secondNav.style.transitionDuration = "600ms";
-        secondNav.style.left = "-10000px";
-    });
 }
+
+});
+
+mobileMenu.addEventListener("click", () => {
+    secondNav.style.transitionDuration = "600ms";
+    secondNav.style.left = 0;
+});
+
+closeBtn.addEventListener("click", () => {
+    secondNav.style.transitionDuration = "600ms";
+    secondNav.style.left = "-10000px";
+});
